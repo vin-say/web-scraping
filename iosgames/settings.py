1c2324267a8a2e28f9302fe948c3f80c0e6943c0
vin-say <vincent.sayseng@gmail.com>
2019-10-04 15:19:29 -0400

    Combination of delays and concurrency requests that don't trigger 403

--- iosgames/settings.py
+++ iosgames/settings.py
@@ -8,6 +8,8 @@
 #     https://doc.scrapy.org/en/latest/topics/settings.html
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+from fake_useragent import UserAgent
+
 
 BOT_NAME = 'iosgames'
 
@@ -19,18 +21,19 @@ FEED_FORMAT = 'csv'
 FEED_URI = 'iosgames.csv'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'GR Data Science (+https://github.com/vin-say/web-scraping)'
+ua = UserAgent()
+USER_AGENT = ua.random
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -80,7 +83,7 @@ AUTOTHROTTLE_ENABLED = True
 #AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 4.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 16.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
